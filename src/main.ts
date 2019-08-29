6a26ea1ae7a0161c02e803ba0b004a92d6355e1d
Santi Ma-Oun <pandamaru1331@gmail.com>
2019-08-29 10:58:17 +0700

    - [docs] Install and using Swagger API document plugin

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -10,6 +11,14 @@ async function bootstrap() {
     whitelist: true,
     transform: true,
   }));
+
+  const options = new DocumentBuilder()
+    .setTitle('Senna Test')
+    .setDescription('Senna Test')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
